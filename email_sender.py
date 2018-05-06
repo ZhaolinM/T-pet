03aa7cd8bb8ee2101d887d024e6db5845f30a03b
ZhaolinM <37645444+ZhaolinM@users.noreply.github.com>
2018-05-06 21:18:01 +1000

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -12,15 +12,15 @@ now = datetime.datetime.now()
 check_date = now+datetime.timedelta(days=1)
 print('Today is:'+str(now)[0:10])
 print('Check for booking at:'+str(check_date)[0:10])
-
-raw_result = list(Booking.objects.filter(time=str(check_date)[0:10],notified=False))
-email_address = list(map(lambda x:x.customer.username,raw_result))
 def send_email(receiver_addresss):
     s = smtplib.SMTP('smtp.gmail.com',587)
     s.starttls()
     s.login('gmail_account','gmail_password')
-    message = "You have a booking with us"
+    message = "You have a booking with us in 24 hours."
     s.sendmail('gmail_account',receiver_addresss,message)
     s.quit()
     print('email send to: '+receiver_addresss)
-send_email(email_address[0])
+raw_result = list(Booking.objects.filter(time=str(check_date)[0:10],notified=False))
+email_address = list(map(lambda x:x.customer.username,raw_result))
+for address in email_address:
+    send_email(address)
